642546268e5a16fed9869b7f5b3a450cb9470a9f
AlienForOtherPlanets <AlienForotherPlanets@users.noreply.github.com>
2022-09-12 14:48:47 +0530

    some changes

---
+++ Frontend/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 8000;
+
+
+app.use("/", express.static(path.join(__dirname, "./")));
+
+app.use("/", require("./route/router"));
+
+
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, './views/'))
+
+
+app.listen(port, () => {
+    console.log(`server is running  at http://localhost:${port}/`);
+})
