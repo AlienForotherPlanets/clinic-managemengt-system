8358f1fd78eef09eb293a51359fa7395f8b0aa39
AlienForOtherPlanets <AlienForotherPlanets@users.noreply.github.com>
2022-09-26 13:44:55 +0530

    change in main

--- Backend/config/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://localhost:27017/SnehiClinic").then(() => {
-    console.log("Database is connected!");
-}).catch((err) => {
-    console.log("error");
-})
